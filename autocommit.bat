c66fa5a5c119134e5ec592cf697bf79263ed9d6e
tncks <26240553+tncks@users.noreply.github.com>
2022-04-03 00:09:32 +0900

    new file added.

--- autocommit.bat
+++ autocommit.bat
@@ -2,4 +2,4 @@ echo a>> a.txt
 git add *
 git config --global user.signingkey 8932b1a1f6416ba403f4176e4f471081479c7199                          
 git commit -S -m "new file added." 
-
+"C:\Program Files\Git\bin\sh.exe" --login -i -- S:\TEMP\test.sh
--- hi.bat
+++ hi.bat
@@ -1,6 +1,1 @@
-"C:\Program Files\Git\bin\sh.exe" --login
-timeout 2
-
-
-git config --list
-
+"C:\Program Files\Git\bin\sh.exe" --login -i -- S:\TEMP\test.sh
