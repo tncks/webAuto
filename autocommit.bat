9f96237ce6a50b802fe00dab0a3dd944ce866975
tncks <26240553+tncks@users.noreply.github.com>
2022-04-02 22:22:13 +0900

    new file added.

--- autocommit.bat
+++ autocommit.bat
@@ -4,4 +4,6 @@ git config --global user.signingkey 8932b1a1f6416ba403f4176e4f471081479c7199
 git commit -S -m "new file added."
 git remote remove origin
 git remote add origin https://github.com/tncks/webAuto.git      
-git push origin master                   
+git push origin master
+tncks
+Piloom01!@#$%                   
