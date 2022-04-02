14b8d750ad2d7fed4847b90f3bb7be824b161e24
tncks <26240553+tncks@users.noreply.github.com>
2022-04-02 22:47:37 +0900

    new file added.

--- autocommit.bat
+++ autocommit.bat
@@ -2,4 +2,4 @@ echo a>> a.txt
 git add *
 git config --global user.signingkey 8932b1a1f6416ba403f4176e4f471081479c7199                          
 git commit -S -m "new file added."      
-git push 'git@github.com:tncks/webAuto.git' master
+git push https://tncks@github.com/tncks/webAuto.git master
